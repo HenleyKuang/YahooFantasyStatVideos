11bf9cfbad48d5a0ffcf2fa48d8b371838071d0b
Henley Kuang <henleyk@google.com>
2021-10-29 13:07:29 -0700

    chrome extension - update content js to fetch a single request of videos

--- chromeextension/content.js
+++ chromeextension/content.js
@@ -1,3 +1,5 @@
+const API_HOST = "https://yahoo-fantasy-bball-stat-video.herokuapp.com"
+
 console.log("testing something!");
 var div = document.createElement('div');
 var label = document.createElement('span');
@@ -5,3 +7,29 @@ label.textContent = "Hello, world";
 div.appendChild(label);
 document.body.appendChild(div);
 console.log("I did something!");
+
+
+async function getVideos() {
+    let url = 'users.json';
+    try {
+        let url = new URL(`${API_HOST}/playervideos`),
+        params = {
+            playerName: "De'Anthony Melton",
+            teamAbbreviation:"MEM",
+            gameDate: "2021-10-26",
+            statType: "STL",
+        }
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+        let res = await fetch(url, {method: 'GET'})
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function logVideoResults() {
+    let vidoeResults = await getVideos()
+    console.log(vidoeResults)
+}
+
+logVideoResults()
