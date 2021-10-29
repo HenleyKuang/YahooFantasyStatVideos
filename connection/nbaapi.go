9600412d3cddded1f8f0e62844ab313b775782aa
Henley Kuang <henleyk@google.com>
2021-10-28 22:13:16 -0700

    Add fetching player videos to nbaapi connection module

--- connection/nbaapi.go
+++ connection/nbaapi.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	playerIndexAPI = "https://stats.nba.com/stats/leaguedashplayerstats?"
+	playerIndexAPI  = "https://stats.nba.com/stats/leaguedashplayerstats?"
+	playerVideosAPI = "https://stats.nba.com/stats/videodetailsasset?"
 )
 
 type resultSet struct {
@@ -93,21 +94,7 @@ func (c *Client) GetPlayerIndex(season string) (map[string]*PlayerIndexResult, e
 		q.Add(qName, qValue)
 	}
 	req.URL.RawQuery = q.Encode()
-
-	req.Header.Set("name", "value")
-	req.Header.Set("Accept", "application/json, text/plain, */*")
-	req.Header.Set("x-nba-stats-token", "true")
-	// req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
-	req.Header.Set("x-nba-stats-origin", "stats")
-	req.Header.Set("sec-ch-ua-platform", "Linux")
-	req.Header.Set("Origin", "https://www.nba.com")
-	req.Header.Set("Sec-Fetch-Site", "same-site")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "https://www.nba.com/")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
-	// req.Header.Set("If-Modified-Since", "Thu, 28 Oct 2021 06:32:33 GMT")
+	setRequestHeaders(&req.Header)
 
 	resp, err := c.httpClient.Do(req)
 
@@ -160,4 +147,119 @@ func (c *Client) GetPlayerIndex(season string) (map[string]*PlayerIndexResult, e
 // Close closes the underlying http connection.
 func (c *Client) Close() {
 	c.httpClient.CloseIdleConnections()
+}
+
+// GetPlayerVideos fetches for the videos for a particular player's stats.
+func (c *Client) GetPlayerVideos(season string, gameID string, teamID int, playerID int, statType string) (map[string]string, error) {
+	req, err := http.NewRequest("GET", playerVideosAPI, nil)
+
+	if err != nil {
+		log.Printf("Errored creating NewRequest. err: %v\n", err)
+		return nil, err
+	}
+	q := req.URL.Query()
+	params := map[string]string{}
+	params["AheadBehind"] = ""
+	params["CFID"] = ""
+	params["CFPARAMS"] = ""
+	params["ClutchTime"] = ""
+	params["Conference"] = ""
+	params["ContextFilter"] = ""
+	params["ContextMeasure"] = statType // "STL"
+	params["DateFrom"] = ""
+	params["DateTo"] = ""
+	params["Division"] = ""
+	params["EndPeriod"] = "10"
+	params["EndRange"] = "28800"
+	params["GROUP_ID"] = ""
+	params["GameEventID"] = ""
+	params["GameID"] = gameID // "0022100040"
+	params["GameSegment"] = ""
+	params["GroupID"] = ""
+	params["GroupMode"] = ""
+	params["GroupQuantity"] = "5"
+	params["LastNGames"] = "0"
+	params["LeagueID"] = "00"
+	params["Location"] = ""
+	params["Month"] = "0"
+	params["OnOff"] = ""
+	params["OpponentTeamID"] = "0"
+	params["Outcome"] = ""
+	params["PORound"] = "0"
+	params["Period"] = "0"
+	params["PlayerID"] = fmt.Sprint(playerID) // "1629001"
+	params["PlayerID1"] = ""
+	params["PlayerID2"] = ""
+	params["PlayerID3"] = ""
+	params["PlayerID4"] = ""
+	params["PlayerID5"] = ""
+	params["PlayerPosition"] = ""
+	params["PointDiff"] = ""
+	params["Position"] = ""
+	params["RangeType"] = "0"
+	params["RookieYear"] = ""
+	params["Season"] = season // "2021-22"
+	params["SeasonSegment"] = ""
+	params["SeasonType"] = "Regular Season"
+	params["ShotClockRange"] = ""
+	params["StartPeriod"] = "1"
+	params["StartRange"] = "0"
+	params["StarterBench"] = ""
+	params["TeamID"] = fmt.Sprint(teamID) // "1610612763"
+	params["VsConference"] = ""
+	params["VsDivision"] = ""
+	params["VsPlayerID1"] = ""
+	params["VsPlayerID2"] = ""
+	params["VsPlayerID3"] = ""
+	params["VsPlayerID4"] = ""
+	params["VsPlayerID5"] = ""
+	params["VsTeamID"] = ""
+
+	for qName, qValue := range params {
+		q.Add(qName, qValue)
+	}
+	req.URL.RawQuery = q.Encode()
+	setRequestHeaders(&req.Header)
+
+	resp, err := c.httpClient.Do(req)
+
+	if err != nil {
+		fmt.Printf("Errored when sending request to the server. err: %v\n", err)
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		err := fmt.Errorf("Status code is not 200 OK. It's %s", resp.Status)
+		fmt.Printf("Errored when sending request to the server. err: %v\n", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("Errored when reading response body. err: %v\n", err)
+		return nil, err
+	}
+
+	fmt.Println(string(responseData))
+
+	return nil, nil
+}
+
+func setRequestHeaders(header *http.Header) {
+	header.Set("name", "value")
+	header.Set("Accept", "application/json, text/plain, */*")
+	header.Set("x-nba-stats-token", "true")
+	// header.Set("sec-ch-ua-mobile", "?0")
+	header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
+	header.Set("x-nba-stats-origin", "stats")
+	header.Set("sec-ch-ua-platform", "Linux")
+	header.Set("Origin", "https://www.nba.com")
+	header.Set("Sec-Fetch-Site", "same-site")
+	header.Set("Sec-Fetch-Mode", "cors")
+	header.Set("Sec-Fetch-Dest", "empty")
+	header.Set("Referer", "https://www.nba.com/")
+	header.Set("Accept-Language", "en-US,en;q=0.9")
+	// header.Set("If-Modified-Since", "Thu, 28 Oct 2021 06:32:33 GMT")
 }
--- playground/nbaapi_playground.go
+++ playground/nbaapi_playground.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"main/connection"
 )
 
@@ -9,6 +8,11 @@ func main() {
 	nbaClient := connection.New(nil)
 	defer nbaClient.Close()
 
-	playerResults, _ := nbaClient.GetPlayerIndex("2021-22")
-	fmt.Printf("%d\n", playerResults["MEM:Desmond Bane"].PlayerID)
+	// playerResults, _ := nbaClient.GetPlayerIndex("2021-22")
+	// fmt.Printf("%d\n", playerResults["MEM:Desmond Bane"].PlayerID)
+
+	playerID := 1629001    // Melton
+	teamID := 1610612763   // MEM
+	gameID := "0022100040" // MEM vs GSW on 10/28/2021
+	nbaClient.GetPlayerVideos("2021-22", gameID, teamID, playerID, "STL")
 }
