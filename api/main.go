044032c10210f3f92194f9b6c3a1dd81cdc87d66
Henley Kuang <henleyk@google.com>
2021-10-28 22:35:53 -0700

    Create /playervideos api endpoint to get list of videos of a player's stat in a game

--- api/main.go
+++ api/main.go
@@ -21,9 +21,19 @@ func playerIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: playerIndex")
 }
 
+func playerVideos(w http.ResponseWriter, r *http.Request) {
+	playerID := 1629001    // Melton
+	teamID := 1610612763   // MEM
+	gameID := "0022100040" // MEM vs GSW on 10/28/2021
+	playerVideoResults, _ := nbaClient.GetPlayerVideos("2021-22", gameID, teamID, playerID, "STL")
+	json.NewEncoder(w).Encode(playerVideoResults)
+	fmt.Println("Endpoint Hit: playervideos")
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/playerindex", playerIndex)
+	http.HandleFunc("/playervideos", playerVideos)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
