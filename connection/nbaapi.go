ff098ac6a8fd49d2f21a43421e4a12ba84bba7a2
Henley Kuang <henleyk@google.com>
2021-10-28 20:52:53 -0700

    change GetPlayerIndex method argument to only require season value

--- connection/nbaapi.go
+++ connection/nbaapi.go
@@ -28,7 +28,8 @@ func New(httpClient *http.Client) *Client {
 }
 
 // GetPlayerIndex returns a list of all current nba players with metadata such as player name, player id, team name, team id, etc.
-func (c *Client) GetPlayerIndex(params map[string]string) (map[string]interface{}, error) {
+// Parameter season expects an nba season represented as a string. e.g. "2021-22"
+func (c *Client) GetPlayerIndex(season string) (map[string]interface{}, error) {
 	req, err := http.NewRequest("GET", playerIndexAPI, nil)
 
 	if err != nil {
@@ -37,6 +38,34 @@ func (c *Client) GetPlayerIndex(params map[string]string) (map[string]interface{
 	}
 
 	q := req.URL.Query()
+	params := map[string]string{}
+	params["DateFrom"] = ""
+	params["DateTo"] = ""
+	params["GameScope"] = ""
+	params["GameSegment"] = ""
+	params["LastNGames"] = "0"
+	params["LeagueID"] = "00"
+	params["Location"] = ""
+	params["MeasureType"] = "Base"
+	params["Month"] = "0"
+	params["OpponentTeamID"] = "0"
+	params["PORound"] = "0"
+	params["PaceAdjust"] = "N"
+	params["PerMode"] = "PerGame"
+	params["Period"] = "0"
+	params["PlayerExperience"] = ""
+	params["PlayerPosition"] = ""
+	params["PlusMinus"] = "N"
+	params["Outcome"] = ""
+	params["Rank"] = "N"
+	params["Season"] = season // "2021-22"
+	params["SeasonSegment"] = ""
+	params["SeasonType"] = "Regular Season"
+	params["StarterBench"] = ""
+	params["TeamID"] = "0"
+	params["TwoWay"] = "0"
+	params["VsConference"] = ""
+	params["VsDivision"] = ""
 	for qName, qValue := range params {
 		q.Add(qName, qValue)
 	}
@@ -56,6 +85,7 @@ func (c *Client) GetPlayerIndex(params map[string]string) (map[string]interface{
 	req.Header.Set("Referer", "https://www.nba.com/")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
 	// req.Header.Set("If-Modified-Since", "Thu, 28 Oct 2021 06:32:33 GMT")
+
 	resp, err := c.httpClient.Do(req)
 
 	if err != nil {
--- playground/nbaapi_playground.go
+++ playground/nbaapi_playground.go
@@ -4,35 +4,7 @@ import "main/connection"
 
 func main() {
 	nbaClient := connection.New(nil)
+	defer nbaClient.Close()
 
-	params := map[string]string{}
-	params["DateFrom"] = ""
-	params["DateTo"] = ""
-	params["GameScope"] = ""
-	params["GameSegment"] = ""
-	params["LastNGames"] = "0"
-	params["LeagueID"] = "00"
-	params["Location"] = ""
-	params["MeasureType"] = "Base"
-	params["Month"] = "0"
-	params["OpponentTeamID"] = "0"
-	params["PORound"] = "0"
-	params["PaceAdjust"] = "N"
-	params["PerMode"] = "PerGame"
-	params["Period"] = "0"
-	params["PlayerExperience"] = ""
-	params["PlayerPosition"] = ""
-	params["PlusMinus"] = "N"
-	params["Outcome"] = ""
-	params["Rank"] = "N"
-	params["Season"] = "2021-22"
-	params["SeasonSegment"] = ""
-	params["SeasonType"] = "Regular Season"
-	params["StarterBench"] = ""
-	params["TeamID"] = "0"
-	params["TwoWay"] = "0"
-	params["VsConference"] = ""
-	params["VsDivision"] = ""
-
-	nbaClient.GetPlayerIndex(params)
+	nbaClient.GetPlayerIndex("2021-22")
 }
