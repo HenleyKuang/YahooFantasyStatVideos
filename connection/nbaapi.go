29380ff5e5403c34f09435b6a72f916cb478cb54
Henley Kuang <henleyk@google.com>
2021-10-28 20:49:26 -0700

    Add headers into nba api module & create class to hold the http connection

--- connection/nbaapi.go
+++ connection/nbaapi.go
@@ -11,9 +11,24 @@ const (
 	playerIndexAPI = "https://stats.nba.com/stats/leaguedashplayerstats?"
 )
 
+// Client holds the http connection.
+type Client struct {
+	httpClient *http.Client
+}
+
+// New creates a new Client object.
+func New(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		c := &http.Client{}
+		httpClient = c
+	}
+	return &Client{
+		httpClient: httpClient,
+	}
+}
+
 // GetPlayerIndex returns a list of all current nba players with metadata such as player name, player id, team name, team id, etc.
-func GetPlayerIndex(params map[string]string) (map[string]interface{}, error) {
-	client := &http.Client{}
+func (c *Client) GetPlayerIndex(params map[string]string) (map[string]interface{}, error) {
 	req, err := http.NewRequest("GET", playerIndexAPI, nil)
 
 	if err != nil {
@@ -26,9 +41,22 @@ func GetPlayerIndex(params map[string]string) (map[string]interface{}, error) {
 		q.Add(qName, qValue)
 	}
 	req.URL.RawQuery = q.Encode()
-	fmt.Println(req.URL.RawPath)
 
-	resp, err := client.Do(req)
+	req.Header.Set("name", "value")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("x-nba-stats-token", "true")
+	// req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
+	req.Header.Set("x-nba-stats-origin", "stats")
+	req.Header.Set("sec-ch-ua-platform", "Linux")
+	req.Header.Set("Origin", "https://www.nba.com")
+	req.Header.Set("Sec-Fetch-Site", "same-site")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://www.nba.com/")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+	// req.Header.Set("If-Modified-Since", "Thu, 28 Oct 2021 06:32:33 GMT")
+	resp, err := c.httpClient.Do(req)
 
 	if err != nil {
 		fmt.Printf("Errored when sending request to the server. err: %v\n", err)
@@ -42,4 +70,9 @@ func GetPlayerIndex(params map[string]string) (map[string]interface{}, error) {
 	fmt.Println(string(responseData))
 
 	return nil, nil
+}
+
+// Close closes the underlying http connection.
+func (c *Client) Close() {
+	c.httpClient.CloseIdleConnections()
 }
--- playground/nbaapi_playground.go
+++ playground/nbaapi_playground.go
@@ -3,9 +3,16 @@ package main
 import "main/connection"
 
 func main() {
+	nbaClient := connection.New(nil)
+
 	params := map[string]string{}
+	params["DateFrom"] = ""
+	params["DateTo"] = ""
+	params["GameScope"] = ""
+	params["GameSegment"] = ""
 	params["LastNGames"] = "0"
 	params["LeagueID"] = "00"
+	params["Location"] = ""
 	params["MeasureType"] = "Base"
 	params["Month"] = "0"
 	params["OpponentTeamID"] = "0"
@@ -13,11 +20,19 @@ func main() {
 	params["PaceAdjust"] = "N"
 	params["PerMode"] = "PerGame"
 	params["Period"] = "0"
+	params["PlayerExperience"] = ""
+	params["PlayerPosition"] = ""
 	params["PlusMinus"] = "N"
+	params["Outcome"] = ""
 	params["Rank"] = "N"
 	params["Season"] = "2021-22"
-	params["SeasonType"] = "Regular+Season"
+	params["SeasonSegment"] = ""
+	params["SeasonType"] = "Regular Season"
+	params["StarterBench"] = ""
 	params["TeamID"] = "0"
 	params["TwoWay"] = "0"
-	connection.GetPlayerIndex(params)
+	params["VsConference"] = ""
+	params["VsDivision"] = ""
+
+	nbaClient.GetPlayerIndex(params)
 }
